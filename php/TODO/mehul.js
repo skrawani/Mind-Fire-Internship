9be273a728e95c7d508db200c84c50a325e0ec99
Sachin Kumar Rawani <skrawani1999@gmail.com>
2021-03-26 18:15:26 +0530

    removed temporary files

--- php/TODO/mehul.js
+++
@@ -1,7 +0,0 @@
-function calculateTotalPrice() {
-  let price = document.getElementById("price").value;
-  let qty = document.getElementById("qty").value;
-  document.getElementById("totalprice").innerHTML =
-    parseFloat(price) * parseInt(qty);
-  return false;
-}
